536c6ac972335ab7bd022293a119a57560c68a32
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2024-07-27 12:23:17 -0300

    associacoes models now accepts blank columns

--- app/apps/associacoes/models.py
+++ app/apps/associacoes/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Associacao(models.Model):
-    nome_fantasia = models.CharField(max_length=254)
-    razao_social = models.CharField(max_length=254)
-    cnpj = models.BigIntegerField()
-    telefone = models.CharField(max_length=10)
-    email = models.EmailField(max_length=254)
-    presidente = models.CharField(max_length=254)
+    nome_fantasia = models.CharField(max_length=254, blank=True, null=True)
+    razao_social = models.CharField(max_length=254, blank=True, null=True)
+    cnpj = models.BigIntegerField( blank=True, null=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(max_length=254 , blank=True, null=True)
+    presidente = models.CharField(max_length=254, blank=True, null=True)
     # rede_sociais = models.ForeignKey(RedesSociais, on_delete=models.DO_NOTHING)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
